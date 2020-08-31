5b1cba8ff8e7abaa795e7b1ebd6c4ba4eddd744f
Tyler Conklin <tylerconklin@dhcp-10-142-194-141.uawifi.arizona.edu>
2020-08-31 11:57:24 -0700

    Add lecture 05 Examples

--- src/MutualFriends.java
+++ src/MutualFriends.java
@@ -9,7 +9,23 @@ import java.util.Set;
 public class MutualFriends {
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(createFriendMap("TestCases/friendList"));
+        Map<String, Set<String>> friendMap = createFriendMap(
+                "TestCases/friendList");
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            System.out.println("Insert first name: ");
+            String first = userInput.nextLine();
+            System.out.println("Insert second name: ");
+            String second = userInput.nextLine();
+            if (first.equals("") || second.equals("")) {
+                break;
+            }
+
+            Set<String> friends = friendMap.get(first);
+            friends.retainAll(friendMap.get(second));
+            System.out.println(friends);
+        }
     }
 
     public static Map<String, Set<String>> createFriendMap(String filename)
---
+++ src/PrintReverse.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class PrintReverse {
+
+    public static void main(String[] args) {
+        reverse("Hello my name is Inigo Montoya");
+    }
+
+    public static void reverse(String str) {
+        String[] words = str.split(" ");
+
+        Stack<String> stringStack = new Stack<String>();
+        for (String word : words) {
+            stringStack.push(word);
+        }
+
+        while (!stringStack.empty()) {
+            System.out.print(stringStack.pop() + " ");
+        }
+    }
+
+}
