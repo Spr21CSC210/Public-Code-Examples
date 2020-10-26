47b3d69cb878d2994f97935d5e59787523f742b4
Tyler Conklin <tylerconklin@dhcp-10-142-209-195.uawifi.arizona.edu>
2020-10-26 14:42:15 -0700

    Exception examples.

---
+++ src/midterm3/StackHeap.java
@@ -0,0 +1,20 @@
+package midterm3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackHeap {
+
+    public static void main(String[] args) {
+        int x = 6;
+        Student s = new Student(...);
+        someFunc();
+        int y = -4;
+    }
+
+    private static void someFunc() {
+        List<Integer> list = new ArrayList<Integer>();
+        int x = 17;
+    }
+
+}
