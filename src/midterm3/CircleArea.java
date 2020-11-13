8ced57254626f7be66d21c7deb0f014a3a2aa93e
Tyler Conklin <tconklin@alumni.stanford.edu>
2020-11-13 12:19:40 -0700

    Add backtracking and memoization examples.

---
+++ src/midterm3/CircleArea.java
@@ -0,0 +1,24 @@
+package midterm3;
+
+// 2. We learned about static imports.
+//  This allows you to use PI and pow in main without qualifying them with their
+//      full names.
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+
+public class CircleArea {
+
+    public static void main(String[] args) {
+        // 1. We learned that you never need to use imports. You can always
+        // qualify the full name like below.
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        Double radius = Double.valueOf(scanner.nextLine());
+
+        Double area = PI * pow(radius, 2);
+
+        System.out.println(area);
+
+    }
+
+}
