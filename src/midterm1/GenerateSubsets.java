9770f7fc6f0b140e46f5647a804e0146b6d05081
Tyler Conklin <tconklin@alumni.stanford.edu>
2021-02-05 13:42:00 -0700

    Added subsets and subsequence examples.

--- src/midterm1/GenerateSubsets.java
+++ src/midterm1/GenerateSubsets.java
@@ -7,7 +7,18 @@ public class GenerateSubsets {
     }
 
     public static void genSubsets(String s) {
+        genSubsetsHelper(s, "");
+    }
 
+    public static void genSubsetsHelper(String s, String soFar) {
+        if (s.isEmpty()) {
+            System.out.println(soFar);
+        } else {
+            char ch = s.charAt(0);
+            genSubsetsHelper(s.substring(1), soFar + ch);
+
+            genSubsetsHelper(s.substring(1), soFar);
+        }
     }
 
 }
--- src/midterm1/IsSubsequence.java
+++ src/midterm1/IsSubsequence.java
@@ -5,9 +5,21 @@ public class IsSubsequence {
         System.out.println(isSubsequence("computer", "core"));
         System.out.println(isSubsequence("computer", "cope"));
         System.out.println(isSubsequence("computer", "computer"));
+        System.out.println(isSubsequence("", ""));
     }
 
     public static boolean isSubsequence(String s1, String s2) {
+        if (s2.isEmpty()) {
+            return true;
+        }
+        if (s1.isEmpty()) {
+            return false;
+        }
 
+        if (s1.charAt(0) == s2.charAt(0)) {
+            return isSubsequence(s1.substring(1), s2.substring(1));
+        } else {
+            return isSubsequence(s1.substring(1), s2);
+        }
     }
 }
