d7e8fa94651f1a4d23c59a1c864f2a204c532cb8
Tyler Conklin <tconklin@alumni.stanford.edu>
2021-03-17 09:59:47 -0700

    Add StackHeap Example.

---
+++ src/midterm3/StackHeap.java
@@ -0,0 +1,29 @@
+package midterm3;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class StackHeap {
+
+    public static void main(String[] args) {
+        int x = 6;
+        Student s = new Student(...);
+        someFunc();
+        int y = -4;
+        /* -------------------------------------- */
+        Set<String> ex = anotherFunc();
+    }
+
+    private static void someFunc() {
+        List<Integer> list = new ArrayList<Integer>();
+        int x = 17;
+    }
+
+    private static Set<String> anotherFunc() {
+        Set<String> wilco = new HashSet<String>();
+        return wilco;
+    }
+
+}
